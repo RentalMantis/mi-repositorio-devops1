9eabf3af6c2fd2507006ecca64e220101b84b6ad
Alejandro <rentalmantis@gmail.com>
2025-04-25 23:30:17 -0600

    Actualizacion

--- app/app.py
+++ app/app.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask, jsonify
 
 app = Flask(__name__)
@@ -8,5 +7,6 @@ def home():
     return jsonify({"message": "¡Hola desde Flask en Docker!"})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=5000)
+    
+    
